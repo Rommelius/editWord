ccef3b011ecc6d11296ea27fecd0a3c4a212730d
Rommel Lapuz <rommel.lapuz@hotmail.com>
2017-03-03 15:55:34 +1000

    First Initial Commit

--- editWord/Form1.Designer.cs
+++ editWord/Form1.Designer.cs
@@ -28,12 +28,304 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.submit_btn = new MetroFramework.Controls.MetroButton();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel10 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel11 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel12 = new MetroFramework.Controls.MetroLabel();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // submit_btn
+            // 
+            this.submit_btn.Location = new System.Drawing.Point(455, 580);
+            this.submit_btn.Name = "submit_btn";
+            this.submit_btn.Size = new System.Drawing.Size(101, 42);
+            this.submit_btn.TabIndex = 0;
+            this.submit_btn.Text = "SUBMIT";
+            this.submit_btn.UseSelectable = true;
+            this.submit_btn.Click += new System.EventHandler(this.submit_btn_Click);
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(24, 241);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel6.TabIndex = 3;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(-1, 5);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(85, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Prepared By:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(347, 5);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(39, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "Date:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(-1, 27);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(61, 19);
+            this.metroLabel3.TabIndex = 4;
+            this.metroLabel3.Text = "Quote #:";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(-1, 49);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(85, 19);
+            this.metroLabel4.TabIndex = 5;
+            this.metroLabel4.Text = "Customer ID:";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(317, 28);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(69, 19);
+            this.metroLabel5.TabIndex = 6;
+            this.metroLabel5.Text = "Valid Until:";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(0, 117);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(61, 19);
+            this.metroLabel7.TabIndex = 7;
+            this.metroLabel7.Text = "Location:";
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(-1, 139);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(100, 19);
+            this.metroLabel8.TabIndex = 8;
+            this.metroLabel8.Text = "Contact Person:";
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(275, 117);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(111, 19);
+            this.metroLabel9.TabIndex = 9;
+            this.metroLabel9.Text = "Contact Number:";
+            // 
+            // metroLabel10
+            // 
+            this.metroLabel10.AutoSize = true;
+            this.metroLabel10.Location = new System.Drawing.Point(-1, 161);
+            this.metroLabel10.Name = "metroLabel10";
+            this.metroLabel10.Size = new System.Drawing.Size(108, 19);
+            this.metroLabel10.TabIndex = 10;
+            this.metroLabel10.Text = "Contact Address:";
+            // 
+            // metroLabel11
+            // 
+            this.metroLabel11.AutoSize = true;
+            this.metroLabel11.Location = new System.Drawing.Point(293, 139);
+            this.metroLabel11.Name = "metroLabel11";
+            this.metroLabel11.Size = new System.Drawing.Size(93, 19);
+            this.metroLabel11.TabIndex = 11;
+            this.metroLabel11.Text = "Contact Email:";
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroLabel12);
+            this.metroPanel1.Controls.Add(this.textBox6);
+            this.metroPanel1.Controls.Add(this.textBox5);
+            this.metroPanel1.Controls.Add(this.textBox4);
+            this.metroPanel1.Controls.Add(this.textBox3);
+            this.metroPanel1.Controls.Add(this.textBox2);
+            this.metroPanel1.Controls.Add(this.textBox10);
+            this.metroPanel1.Controls.Add(this.textBox9);
+            this.metroPanel1.Controls.Add(this.textBox8);
+            this.metroPanel1.Controls.Add(this.textBox7);
+            this.metroPanel1.Controls.Add(this.textBox1);
+            this.metroPanel1.Controls.Add(this.metroLabel11);
+            this.metroPanel1.Controls.Add(this.metroLabel10);
+            this.metroPanel1.Controls.Add(this.metroLabel9);
+            this.metroPanel1.Controls.Add(this.metroLabel8);
+            this.metroPanel1.Controls.Add(this.metroLabel7);
+            this.metroPanel1.Controls.Add(this.metroLabel5);
+            this.metroPanel1.Controls.Add(this.metroLabel4);
+            this.metroPanel1.Controls.Add(this.metroLabel3);
+            this.metroPanel1.Controls.Add(this.metroLabel2);
+            this.metroPanel1.Controls.Add(this.metroLabel1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(24, 78);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(532, 492);
+            this.metroPanel1.TabIndex = 4;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel12
+            // 
+            this.metroLabel12.AutoSize = true;
+            this.metroLabel12.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel12.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel12.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.metroLabel12.Location = new System.Drawing.Point(0, 82);
+            this.metroLabel12.Name = "metroLabel12";
+            this.metroLabel12.Size = new System.Drawing.Size(161, 25);
+            this.metroLabel12.TabIndex = 13;
+            this.metroLabel12.Text = "Customer Details:";
+            this.metroLabel12.UseCustomForeColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(113, 160);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(158, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(113, 138);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(158, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(113, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(158, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(392, 138);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(135, 20);
+            this.textBox10.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(392, 116);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(135, 20);
+            this.textBox9.TabIndex = 12;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(392, 28);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(135, 20);
+            this.textBox8.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(392, 5);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(135, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(579, 645);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.submit_btn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Nova Biomedical Quote Generator";
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroButton submit_btn;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
+        private MetroFramework.Controls.MetroLabel metroLabel10;
+        private MetroFramework.Controls.MetroLabel metroLabel11;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroLabel metroLabel12;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- editWord/Form1.cs
+++ editWord/Form1.cs
@@ -7,14 +7,163 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Diagnostics;
 
 namespace editWord
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
+        string appRootDir = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void todayDate_Click(object sender, EventArgs e)
+        {
+
+            //date.Text = DateTime.Today.ToShortDateString();
+
+        }
+
+        private void submit_btn_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Continue?", "Please make sure all Microsoft Word is not in use",
+                MessageBoxButtons.OKCancel);
+            switch (result)
+            {
+                case DialogResult.OK:
+                    {
+                        //foreach (Process p in Process.GetProcessesByName("WINWORD"))
+                        //{
+                        //    p.Kill();
+                        //}
+                        CreateWordDocument(appRootDir + "/Reports/Nova Biomedical Quote TEMPLATE.docx", appRootDir + "/Reports/new.docx");
+                        break;
+                    }
+                case DialogResult.Cancel:
+                    {
+                        this.Text = "[Cancel]";
+                        break;
+                    }
+            }
+
+
+        }
+
+        /// <summary>
+        /// This is simply a helper method to find/replace 
+        /// text.
+        /// </summary>
+        /// <param name="WordApp">Word Application to use</param>
+        /// <param name="findText">Text to find</param>
+        /// <param name="replaceWithText">Replacement text</param>
+        private void FindAndReplace(Word.Application WordApp,
+                                    object findText,
+                                    object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object nmatchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            WordApp.Selection.Find.Execute(ref findText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike,
+                ref nmatchAllWordForms, ref forward,
+                ref wrap, ref format, ref replaceWithText,
+                ref replace, ref matchKashida,
+                ref matchDiacritics, ref matchAlefHamza,
+                ref matchControl);
+        }
+
+        private void CreateWordDocument(object fileName,
+                                        object saveAs)
+        {
+            //Set Missing Value parameter - used to represent
+            // a missing value when calling methods through
+            // interop.
+            object missing = System.Reflection.Missing.Value;
+
+            //Setup the Word.Application class.
+            Word.Application wordApp =
+                new Word.Application();
+
+            //Setup our Word.Document class we'll use.
+            Word.Document wDoc = null;
+
+            // Check to see that file exists
+            if (File.Exists((string)fileName))
+            {
+                DateTime today = DateTime.Now;
+
+                object readOnly = false;
+                object isVisible = false;
+
+                //Set Word to be not visible.
+                wordApp.Visible = false;
+
+                //Open the word document
+                wDoc = wordApp.Documents.Open(ref fileName, ref missing,
+                    ref readOnly, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref isVisible, ref missing, ref missing,
+                    ref missing, ref missing);
+
+                // Activate the document
+                wDoc.Activate();
+
+                //// Find Place Holders and Replace them with Values.
+                //this.FindAndReplace(wordApp, "<FIRST>", firstName.Text);
+                //this.FindAndReplace(wordApp, "<LAST>", lastName.Text);
+                //this.FindAndReplace(wordApp, "<COMPANY>", company.Text);
+                //this.FindAndReplace(wordApp, "<DATE>", date.Text);
+                //this.FindAndReplace(wordApp, "<MESSAGE>", messageBox.Text);
+
+                ////Example of writing to the start of a document.
+                //wDoc.Content.InsertBefore("This is at the beginning\r\n\r\n");
+
+                ////Example of writing to the end of a document.
+                //wDoc.Content.InsertAfter("\r\n\r\nThis is at the end");
+            }
+            else
+            {
+                MessageBox.Show("File dose not exist.");
+                return;
+            }
+
+
+            ////Save the document as the correct file name.
+            //wDoc.SaveAs(ref saveAs, ref missing, ref missing, ref missing,
+            //        ref missing, ref missing, ref missing, ref missing,
+            //        ref missing, ref missing, ref missing, ref missing,
+            //        ref missing, ref missing, ref missing, ref missing);
+
+
+            //export to pdf
+            wDoc.ExportAsFixedFormat(appRootDir + "/Reports/test.pdf", Word.WdExportFormat.wdExportFormatPDF);
+
+            //Close the document - you have to do this.
+            wDoc.Close(ref missing, ref missing, ref missing);
+
+            MessageBox.Show("File created.");
         }
     }
 }
